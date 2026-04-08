d316b5600ac9bc115f8cf95bbead053974dbfece
YG_Kim <yeogyeom1@naver.com>
2026-04-08 18:36:06 +0900

    docker-compose.yml 및 Dockerfile 수정: ROS2 및 PX4 서비스 설정 변경, 불필요한 스크립트 삭제

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
-INSTANCE=${PX4_INSTANCE:-0}
+# 인자 우선, 없으면 환경변수, 기본값 0
+INSTANCE=${1:-${PX4_INSTANCE:-0}}
 
-echo "Starting PX4 SITL (instance ${INSTANCE})..."
+echo "Starting PX4 SITL Zenoh (instance ${INSTANCE})..."
 cd /root/PX4-Autopilot
-./build/px4_sitl_zenoh/bin/px4 -i ${INSTANCE}
+
+# ZENOH_ENABLE 파라미터를 자동 설정하는 startup 명령
+./build/px4_sitl_zenoh/bin/px4 -i ${INSTANCE} -c "param set ZENOH_ENABLE 1"
