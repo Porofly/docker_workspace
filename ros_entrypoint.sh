6afa26e1611f8745392c3025c3baa576895f4b0e
YG_Kim <yeogyeom1@naver.com>
2026-04-08 16:33:59 +0900

    Dockerfile 및 entrypoint 스크립트에서 ROS2 Jazzy 환경 변수를 설정하도록 수정

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
-# ROS2 Jazzy 환경
+# ROS2 Jazzy 환경 (dustynv 소스빌드 + apt 패키지 경로)
 source /opt/ros/jazzy/install/setup.bash
+export AMENT_PREFIX_PATH=/opt/ros/jazzy:${AMENT_PREFIX_PATH}
+export LD_LIBRARY_PATH=/opt/ros/jazzy/lib:${LD_LIBRARY_PATH}
+export PATH=/opt/ros/jazzy/bin:${PATH}
 
 # px4_msgs 워크스페이스
 if [ -f /root/ros2_ws/install/local_setup.bash ]; then
