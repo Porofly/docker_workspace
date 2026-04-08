503afab3b2cb3e3387ff10dd421d6a2a90d23ddd
YG_Kim <yeogyeom1@naver.com>
2026-04-08 17:25:25 +0900

    Dockerfile 및 entrypoint 스크립트에서 ROS2 Jazzy 설치 경로 수정 및 불필요한 환경 변수 제거

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e
 
-# ROS2 Jazzy 환경 (dustynv 소스빌드 + apt 패키지 경로)
-source /opt/ros/jazzy/install/setup.bash
-export AMENT_PREFIX_PATH=/opt/ros/jazzy:${AMENT_PREFIX_PATH}
-export LD_LIBRARY_PATH=/opt/ros/jazzy/lib:${LD_LIBRARY_PATH}
-export PATH=/opt/ros/jazzy/bin:${PATH}
+# ROS2 Jazzy 환경
+source /opt/ros/jazzy/setup.bash
 
 # px4_msgs 워크스페이스
 if [ -f /root/ros2_ws/install/local_setup.bash ]; then
