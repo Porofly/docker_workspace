21f88494342fd9b1a59959c7c3a4902da4fcd6e6
YG_Kim <yeogyeom1@naver.com>
2026-04-10 13:33:56 +0900

    PX4 인스턴스 환경 변수 제거 및 시작 스크립트 수정

--- start.sh
+++ start.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -e
 
-# 인자 우선, 없으면 환경변수, 기본값 0
-INSTANCE=${1:-${PX4_INSTANCE:-0}}
-
-echo "Starting PX4 SITL Zenoh (instance ${INSTANCE})..."
+echo "Starting PX4 SITL Zenoh..."
 cd /root/PX4-Autopilot
 
 # rcS 패치: ZENOH_ENABLE 조건 체크를 제거하고 zenoh를 항상 시작
@@ -14,4 +11,4 @@ if ! grep -q "# ZENOH_AUTO_START_PATCHED" "$RCS"; then
     echo "rcS patched for automatic zenoh start"
 fi
 
-./build/px4_sitl_zenoh/bin/px4 -i ${INSTANCE}
+./build/px4_sitl_zenoh/bin/px4
