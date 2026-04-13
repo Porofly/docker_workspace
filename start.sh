70074fd03c416db251aef17d05d39e35583a285d
YG_Kim <yeogyeom1@naver.com>
2026-04-13 17:33:40 +0900

    비교 실험용 DDS 브랜치

--- start.sh
+++ start.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "Starting PX4 SITL Zenoh..."
+echo "Starting PX4 SITL (uXRCE-DDS)..."
 cd /root/PX4-Autopilot
 
-# rcS 패치: ZENOH_ENABLE 조건 체크를 제거하고 zenoh를 항상 시작
-RCS=build/px4_sitl_zenoh/etc/init.d-posix/rcS
-if ! grep -q "# ZENOH_AUTO_START_PATCHED" "$RCS"; then
-    sed -i 's|if param greater -s ZENOH_ENABLE 0|# ZENOH_AUTO_START_PATCHED\nif true|' "$RCS"
-    echo "rcS patched for automatic zenoh start"
-fi
+# uXRCE-DDS 에이전트 연결 설정 (기본값: localhost:8888 UDP)
+# 두 컨테이너 모두 network_mode: host이므로 로컬호스트로 통신
+export UXRCE_DDS_AG_IP=2130706433   # 127.0.0.1 (int32)
+export UXRCE_DDS_PRT=8888
 
-./build/px4_sitl_zenoh/bin/px4
+./build/px4_sitl_default/bin/px4
